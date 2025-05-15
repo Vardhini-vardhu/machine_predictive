883e1f3cb7b7bcdd8b6b99376bf6d26a2ee82fde
Vardhini-vardhu <132774875+Vardhini-vardhu@users.noreply.github.com>
2025-05-15 09:56:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ st.markdown(
         box-shadow: 0 2px 5px rgba(0,0,0,0.2);
     }
     .input-description {
-        color: #ecf0f1;
+        color: black;
         font-size: 12px;
         margin-top: -10px;
         margin-bottom: 15px;
